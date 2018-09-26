5c07d314121014295549fac2f06a2c3326cd0b9f
Milo Cesar <milocesar1@gmail.com>
2018-09-26 11:55:09 +0200

    Add publish on mix build

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,18 @@
 let mix = require('laravel-mix');
+require('dotenv').config();
+let child_process = require('child_process');
 
+let cmd = process.env.MIX_AFTER_CMD;
+console.log(cmd);
 mix.js('src/resources/assets/js/app.js', 'src/public/js')
     .sass('src/resources/assets/sass/app.scss', 'src/public/css/')
-    .sourceMaps();
+    .sourceMaps()
+    .then(() => {
+        if(cmd != null) {
+            child_process.exec(cmd, function (error, stdout, stderr) {
+                if (error) throw error;
+
+                console.log(stdout, stderr);
+            });
+        }
+    });
