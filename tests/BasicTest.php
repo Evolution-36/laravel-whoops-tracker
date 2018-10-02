104b5d09ba6cf12c8acce87dd3609bdedfed8003
Rien Heuver <rienheuver@gmail.com>
2018-10-02 14:13:04 +0200

    Attempt #6 at fixing travis builds

---
+++ tests/BasicTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Evolution36\WhoopsTracker\Test;
+
+use Orchestra\Testbench\TestCase;
+
+class TestTest extends TestCase
+{
+    public function testTest()
+    {
+        $this->assertTrue(true);
+    }
+}
